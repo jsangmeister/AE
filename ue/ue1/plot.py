6447ffcaa3041a32b92be610af8d89b6ad7453ec
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2020-11-14 14:58:34 +0100

    add smart filter

--- ue/ue1/plot.py
+++ ue/ue1/plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib
-from collections import defaultdict
 import math
+from dataclasses import dataclass
 
 # stolen from matplotlib.ticker.LogFormatterExponent
 class Log2Formatter(matplotlib.ticker.LogFormatter):
@@ -24,28 +24,41 @@ N = [int(pow(2, 18)), int(pow(2, 20)), int(pow(2, 22)), int(pow(2, 24)), int(pow
 M = [64*_k, 128*_k, 256*_k, 1*_m, 2*_m, 4*_m, 8*_m]
 B = [8*_k, 16*_k, 64*_k, 128*_k, 512*_k]
 
-def load(name, data_time, data_n):
+@dataclass
+class Row:
+    q: int
+    n: int
+    m: int
+    b: int
+    time: int
+
+def load(name):
+    arr = []
     with open(f"results-{name}.txt") as f:
         for l in f:
             parts = l.split(" ")
-            q, n, m, b, time = [int(p.strip()) for p in parts]
-            data_time[name][q][m][b].append(time)
-            data_n[name][q][m][b].append(n)
+            arr.append(Row(*(int(p.strip()) for p in parts)))
+    return arr
 
-data_n = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
-data_time = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
+def filter(data, out_key, filter):
+    arr = []
+    for row in data:
+        if any(key in filter and getattr(row, key) != filter[key] for key in ("q", "n", "m", "b")):
+            continue
+        arr.append(getattr(row, out_key))
+    return arr
 
-load("finn", data_time, data_n)
-load("joshua", data_time, data_n)
-load("levin", data_time, data_n)
+data_finn = load("finn")
+data_joshua = load("joshua")
+data_levin = load("levin")
 
+x = filter(data_finn, "n", {"q": 8, "m": 128*_k, "b": 8*_k})
 ax = plt.subplot(1, 1, 1)
-ax.plot(data_n["finn"][8][128*_k][8*_k], data_time["finn"][8][128*_k][8*_k])
-ax.plot(data_n["joshua"][8][128*_k][8*_k], data_time["joshua"][8][128*_k][8*_k])
-ax.plot(data_n["levin"][8][128*_k][8*_k], data_time["levin"][8][128*_k][8*_k])
+ax.plot(x, filter(data_finn, "time", {"q": 8, "m": 128*_k, "b": 8*_k}))
+ax.plot(x, filter(data_joshua, "time", {"q": 8, "m": 128*_k, "b": 8*_k}))
+ax.plot(x, filter(data_levin, "time", {"q": 8, "m": 128*_k, "b": 8*_k}))
 ax.grid()
 ax.legend(["finn", "joshua", "levin"])
 ax.get_xaxis().set_major_formatter(Log2Formatter(base=2))
 ax.set_xticks(N)
-
 plt.show()
